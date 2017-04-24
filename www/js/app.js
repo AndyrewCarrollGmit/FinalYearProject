47a3135710a864d70977e7caf91a38288a401af5
AndyrewCarrollGmit <g00321274@gmit.ie>
2017-04-24 15:16:16 +0100

    Add files via upload

--- www/js/app.js
+++ www/js/app.js
@@ -1,72 +1,106 @@
+// db variable set to null
+var db = null;
 var app = angular.module('starter', ['ionic', 'ngCordova']);
 
-app.run(function ($ionicPlatform) {
+app.run(function ($ionicPlatform, $cordovaSQLite) {
     'use strict';
 	$ionicPlatform.ready(function () {
         if (window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      		// for form inputs)
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+      
+			cordova.plugins.Keyboard.disableScroll(true);
+		}
+		if(window.StatusBar) {
+		  StatusBar.styleDefault();
+		}
+		// Create Database & Table 
+		if (window.cordova) {
+			  db = $cordovaSQLite.openDB({ name: "my.db", location: "default" });
+        	  $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS Language_Stats (Id integer primary key, Language_used text, Date_used date)");	
+			}else{
+			  db = window.openDatabase("my.db", '1', 'my', 1024 * 1024 * 100); // browser
+			  $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS Language_Stats (Id integer primary key, Language_used text, Date_used date)");		
+			  
+			}	
   });
 
 });
 
-app.controller('smsController', ['$scope','$cordovaSms','$http', function($scope,$cordovaSms,$http){
-  
-	
+
+//controls the data  
+app.controller('smsController', ['$scope','$cordovaSms', '$cordovaSQLite', '$http', function($scope,$cordovaSms, $cordovaSQLite, $http){
+  	
  $scope.sms={};
       var options = {
       replaceLineBreaks: false, // true to replace \n by a new line, false by default
       android: {
-       // intent: 'INTENT'  // send SMS with the default SMS app
        intent: ''        // send SMS without open any other app
       }
     };
+
+	// variable 	
+   var returnValue; 
 	
-	
-	var returnValue;
-	
+	// GET Request API from transltr.org 
 	function getRequest(text,to,from){
 	$http({
-  method: 'GET',
-  url: 'http://www.transltr.org/api/translate?text=' + text + '&to=' +to +'&from=' + from
-}).then(function successCallback(response) {
-		returnValue = response.data;
-   	console.log(returnValue.translationText);
-  }, function errorCallback(response) {
-      
-  });
-};
+		  method: 'GET',
+		  url: 'http://www.transltr.org/api/translate?text=' + text + '&to=' +to +'&from=' + from
+		}).then(function successCallback(response) {
+				returnValue = response.data;
+			console.log(returnValue.translationText);
+		  }, function errorCallback(response) {
+		console.log("Fail");
+		  });
+		};
+	// Add Records to the database
+	  function saveStats(lang){
+	  //Save stats to DB
+	  $scope.insert = function() {
+			var query = "INSERT INTO Language_Stats (Language_Used, Date_Used) VALUES (?,?)";
+			$cordovaSQLite.execute(db, query, [lang, '01/01/1999']).then(function(res) {
+				console.log("INSERT ID -> " + res.insertId);
+			}, function (err) {
+				console.error(err);
+			});
+		}
+	  };
 	
-  
+// Function to Send the SMS
   $scope.sendSms=function(){
-   console.log($scope.sms.number);
-   console.log($scope.sms.message);
+	  	 // Test on the console 
+	//console.log($scope.sms.number);
+   	//console.log($scope.sms.message);
 	
-	  alert("SMS has been sent");
-	  
 	  
 	getRequest($scope.sms.message,$scope.sms.language,'en');
-
-    $cordovaSms
-        .send($scope.sms.number,returnValue.translationText, options)
-          .then(function() {
-              // Success! SMS was sent
-              console.log('Success');
-          }, function(error) {
-          // An error occurred
-              console.log(error);
-        });//then
+	   //Get the translated text 
+	saveStats($scope.sms.language); 
+	   
+	  
+	$cordovaSms
+		.send($scope.sms.number,returnValue.translationText, options)
+		  .then(function() {
+			   //SMS was sent
+			  console.log('Success');
+		  }, function(error) {
+		  // Error
+			  console.log(error);
+		});//then
+	 
+	  //Message Alert
+	alert("SMS has been sent: " + returnValue.translationText);  
   }//sendSms
+  
+  
+	
+	  
 
+  
 
+ 
+  
+  
 }])
